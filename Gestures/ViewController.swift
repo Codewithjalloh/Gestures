7b5ef4ea9263b6dee72daa92d85936d994bd225b
D.A Jalloh <info@codewithjalloh.com>
2016-07-06 00:40:57 +0100

    UI and outlet added

--- Gestures/ViewController.swift
+++ Gestures/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var theImage: UIImageView!
+    
+    var netRotation:CGFloat = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        // SWIPE
+        let swipeGestRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGest:")
+        theImage.addGestureRecognizer(swipeGestRight)
+        
+        let swipeGestDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGest:")
+        theImage.addGestureRecognizer(swipeGestDown)
+        
+        let swipeGestUp = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGest:")
+        theImage.addGestureRecognizer(swipeGestUp)
+        
+        let swipeGestLeft = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGest:")
+        theImage.addGestureRecognizer(swipeGestLeft)
+        
+        //ROTATION
+        let rotateGest = UISwipeGestureRecognizer(target: self, action: "rotateGesture:")
+        theImage.addGestureRecognizer(rotateGest)
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
